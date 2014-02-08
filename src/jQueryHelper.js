1f70b7b0ac460c02d2401c6b81166d612fa7cf32
andygup <agup@esri.com>
2014-02-08 12:09:58 -0700

    added debouncing functionality to the map

--- src/jQueryHelper.js
+++ src/jQueryHelper.js
@@ -150,14 +150,14 @@ var jQueryHelper = function(/* Map */ map){
 
     /**
      * Activates the orientation listener and listens for native events.
+     * Handle orientation events to allow for resizing the map and working around
+     * jQuery mobile bugs related to how and when the view settles after such an event
      */
     this.setOrientationListener = function(){
-        //Handle orientation events to allow for resizing the map and working around
-        //jQuery mobile bugs related to how and when the view settles after such an event
         var supportsOrientationChange = "onorientationchange" in window,
             orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
 
-        window.addEventListener(orientationEvent, function () {
+        window.addEventListener(orientationEvent, this.debounceMap(function(){
             if(this._getActivePage() == this.currentPageID){
                 this.recenterOnRotate(400);
                 this.rotatedFlag = false;
@@ -165,8 +165,29 @@ var jQueryHelper = function(/* Map */ map){
             else{
                 this.rotatedFlag = true;
             }
-        }.bind(this), false);
+        },250).bind(this), false);
     }
+
+    /**
+     * Minimize the number of times window readjustment fires a function
+     * http://davidwalsh.name/javascript-debounce-function
+     * @param func
+     * @param wait
+     * @param immediate
+     * @returns {Function}
+     */
+    this.debounceMap = function (func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            }, wait);
+            if (immediate && !timeout) func.apply(context, args);
+        };
+    };
 
     /**
      * Automatically sets new center point in local storage.
